1da1535dbb04de3839c06ed4e628607f5d9b1a01
toddlundwall <87336863+toddlundwall@users.noreply.github.com>
2022-02-21 13:34:54 -0700

    initial commit

---
+++ script.js
@@ -0,0 +1,23 @@
+console.log('h');
+
+
+function scramble(str1, str2) {
+    const arr1 = str1.split('')
+    const arr2 = str2.split('')
+
+
+    const newArray = arr2.map(el => { 
+        if (!arr1.includes(el)) return false
+        arr1.splice(`${arr1.indexOf(el)}`, 1)
+        return el
+        
+    })
+    console.log(newArray);
+    if (newArray.includes(false)) return false
+    else return true
+
+
+}
+
+
+console.log(scramble('rodlw','worrltd'))
